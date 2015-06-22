66a3d40cf90ae0f9fd9ef1708a2b0daf26b28162
zheyuwu <b02902125@ntu.edu.tw>
2015-06-23 00:18:18 +0900

    Add DCT watermark

--- final/draftcode/reader/locate.py
+++ final/draftcode/reader/locate.py
@@ -5,13 +5,12 @@ import time
 H = 1080
 W = 1920
 CH = 720
-CW = 1600
-LOCATE_SIZE = 140
+CW = 1560
+LOCATE_SIZE = 105
 PAD_H = (H - (LOCATE_SIZE / 7 * 8) * 2 - CH) / 2
 PAD_W = (W - (LOCATE_SIZE / 7 * 8) * 2 - CW) / 2
 CPAD_H = PAD_H + LOCATE_SIZE / 7 * 8
 CPAD_W = PAD_W + LOCATE_SIZE / 7 * 8
-
 O_FPS = 30
 T_FPS = 60
 
@@ -113,7 +112,77 @@ def detect_fix(index,sframe):
                 [PAD_W + LOCATE_SIZE / 2,H - PAD_H - LOCATE_SIZE / 2]],dtype = np.float32))
     return cv2.warpPerspective(sframe,mat,(W,H))
 
+def get_alignblock(sframe):
+    frame = cv2.Canny(sframe,100,400)
+    _,conto,hier = cv2.findContours(frame.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    if hier is None:
+        return np.array([])
+    pos = []
+    for idx,h in enumerate(hier[0]):
+        if h[3] != -1:
+            continue
+        co = conto[idx][:,0,:]
+        a = cv2.contourArea(co)
+        if a < 100:
+            continue
+        m = cv2.moments(co)
+        pos.append((m['m10'] / m['m00'],m['m01'] / m['m00']))
+    return np.array(pos)
 
+def align(sframe):
+    post = get_alignblock(sframe[PAD_H:PAD_H + LOCATE_SIZE,CPAD_W - LOCATE_SIZE / 8:W - CPAD_W + LOCATE_SIZE / 8])
+    post += [PAD_H,CPAD_W - LOCATE_SIZE / 8]
+    posb = get_alignblock(sframe[H - PAD_H - LOCATE_SIZE:H - PAD_H,CPAD_W - LOCATE_SIZE / 8:W - CPAD_W + LOCATE_SIZE / 8])
+    posb += [H - PAD_H - LOCATE_SIZE,CPAD_W - LOCATE_SIZE / 8]
+    post = sorted(post,key = lambda x: x[0])
+    posb = sorted(posb,key = lambda x: x[0])
+
+def gs(X,row_vecs = True,norm = True):
+    if not row_vecs:
+        X = X.T
+    Y = X[0:1,:].copy()
+    for i in range(1, X.shape[0]):
+        proj = np.diag((X[i,:].dot(Y.T)/np.linalg.norm(Y,axis=1)**2).flat).dot(Y)
+        Y = np.vstack((Y, X[i,:] - proj.sum(0)))
+    if norm:
+        Y = np.diag(1/np.linalg.norm(Y,axis=1)).dot(Y)
+    if row_vecs:
+        return Y
+    else:
+        return Y.T
+
+def genpat(pattern):
+    d = np.zeros((120,120),dtype = np.float)
+    index = 0
+    for y in range(120):
+        for x in range(120):
+            if (x + y) >= 0 and (x + y) < 30:
+                d[y,x] = pattern[index]
+                index += 1
+    return d
+
+def extract(sframe):
+    data = []
+    for y in range(0,CH,120):
+        for x in range(0,CW,120):
+            ma = 0
+            maidx = 0
+            e = cv2.dct(sframe[y:y + 120,x:x + 120])
+            for idx,d in enumerate(ds):
+                v = abs(np.sum(d * e))
+                if v > ma:
+                    ma = v
+                    maidx = idx
+            data.append(maidx)
+    return data
+
+invc = cv2.VideoCapture('MVI_9072.MOV')
+fps = invc.get(cv2.CAP_PROP_FPS)
+print(fps)
+fourcc = cv2.VideoWriter_fourcc(*'X264')
+fixc = cv2.VideoWriter('fix.avi',fourcc,fps,(CW,CH))
+    
+'''
 chess_b = np.full((16,16),0)
 chess_w = np.full((16,16),255)
 chess = np.concatenate(
@@ -122,14 +191,23 @@ chess = np.concatenate(
 chess_inv = np.concatenate(
         [np.concatenate([chess_w,chess_b] * 16),np.concatenate([chess_b,chess_w] * 16)] * 16,
         axis = 1)
+'''
 
-invc = cv2.VideoCapture('MOV_0024.MP4')
-fps = invc.get(cv2.CAP_PROP_FPS)
-print(fps)
-fourcc = cv2.VideoWriter_fourcc(*'X264')
-fixc = cv2.VideoWriter('fix.avi',fourcc,fps,(CW,CH))
+np.random.seed(23)
+keys = []
+for j in range(256):
+    v = []
+    for i in range((1 + 30) * 30 // 2):
+        v.append(np.random.normal(0,10000))
+    keys.append(v)
+keys = np.array(keys)
+keys = gs(keys) * 1000
+ds = []
+for key in keys:
+    ds.append(genpat(key))
 
 index = 0
+aframe = None
 while invc.isOpened():
     #_,sframe = invc.read()
     #if sframe is None:
@@ -140,35 +218,24 @@ while invc.isOpened():
     bframe = detect_fix(0,invc.read()[1])
     if bframe is None:
         continue
+    #align(bframe)
 
-    bframe = bframe[CPAD_H:H - CPAD_H,CPAD_W:W - CPAD_W].astype(np.uint8)
+    bframe = bframe.astype(np.uint8)
+    bframe = bframe[CPAD_H:H - CPAD_H,CPAD_W:W - CPAD_W]
     bframe = cv2.cvtColor(bframe,cv2.COLOR_RGB2HSV)
-    bframe = bframe[:512,:512,2]
+    bframe = bframe[:,:,2]
+    print(bframe.shape)
 
-    dcta = cv2.dct((chess - 128).astype(np.float))
-    #dframe = (dct / np.max(dct) * 255).astype(np.uint8)
-    dcta[0,0] = 0.0
+    if aframe is not None:
+        cframe = (aframe.astype(np.float) + bframe.astype(np.float)) / 2.0
+        cframe = (bframe.astype(float) - cframe)
 
-    dctb = cv2.dct((bframe - 128).astype(np.float))
-    #dct -= np.min(dct)
-    #pframe = (dct / np.max(dct) * 255).astype(np.uint8)
-
-    print(dcta)
-    print(dctb)
-    print(np.sum(dcta * dctb))
-
-    cv2.imshow('x',bframe)
-    cv2.waitKey(0)
-
-    #bframe = cv2.adaptiveThreshold(bframe,200,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,17,0)
+        print(extract(cframe))
+        
+        cv2.imshow('x',(cframe + 128).astype(np.uint8))
+        cv2.waitKey(0)
 
     aframe = bframe
-
-    #fframe = detect_fix(index,sframe)
-    #if fframe is None:
-    #    continue
-
-    #fixc.write(fframe[CPAD_H:H - CPAD_H,CPAD_W:W - CPAD_W])
 
 fixc.release()
 cv2.destroyAllWindows()
--- final/draftcode/writer/writer.py
+++ final/draftcode/writer/writer.py
@@ -6,7 +6,7 @@ import random
 H = 1080
 W = 1920
 CH = 720
-CW = 1600
+CW = 1560
 LOCATE_SIZE = 105
 PAD_H = (H - (LOCATE_SIZE / 7 * 8) * 2 - CH) / 2
 PAD_W = (W - (LOCATE_SIZE / 7 * 8) * 2 - CW) / 2
@@ -23,6 +23,20 @@ def gen_locate():
     i_locate = cv2.cvtColor(locate,cv2.COLOR_GRAY2RGB)
     return i_locate
 
+def gen_halign():
+    block = np.full((16,16),255,dtype = np.uint8)
+    bar = np.full((16,CW,3),0,dtype = np.uint8)
+    for i in range(0,CW,32):
+        bar[:,i:i + 16,2] = block
+    return cv2.cvtColor(bar,cv2.COLOR_HSV2RGB_FULL)
+
+def gen_valign():
+    block = np.full((16,16),255,dtype = np.uint8)
+    bar = np.full((CH,16,3),0,dtype = np.uint8)
+    for i in range(0,CH,32):
+        bar[i:i + 16,:,2] = block
+    return cv2.cvtColor(bar,cv2.COLOR_HSV2RGB_FULL)
+
 i_frame = cv2.cvtColor(np.full((H,W),0,dtype = np.uint8),cv2.COLOR_GRAY2RGB)
 
 i_locate = gen_locate()
@@ -31,14 +45,22 @@ i_frame[PAD_H + 0:PAD_H + LOCATE_SIZE,W - LOCATE_SIZE - PAD_W:W - PAD_W] = i_loc
 i_frame[H - LOCATE_SIZE - PAD_H:H - PAD_H,PAD_W + 0:PAD_W + LOCATE_SIZE] = i_locate
 i_frame[H - LOCATE_SIZE - PAD_H:H - PAD_H,W - LOCATE_SIZE - PAD_W:W - PAD_W] = i_locate
 
+bar = gen_halign()
+bh,bw,_ = bar.shape
+i_frame[PAD_H + (LOCATE_SIZE - bh) // 2:PAD_H + (LOCATE_SIZE + bh) // 2,CPAD_W:W - CPAD_W] = bar
+i_frame[H - PAD_H - (LOCATE_SIZE + bh) // 2:H - PAD_H - (LOCATE_SIZE - bh) // 2,CPAD_W:W - CPAD_W] = bar
+bar = gen_valign()
+bh,bw,_ = bar.shape
+i_frame[CPAD_H:H - CPAD_H,PAD_W + (LOCATE_SIZE - bw) // 2:PAD_W + (LOCATE_SIZE + bw) // 2] = bar
+i_frame[CPAD_H:H - CPAD_H,W - PAD_W - (LOCATE_SIZE + bw) // 2:W - PAD_W - (LOCATE_SIZE - bw) // 2] = bar
+
 invc = cv2.VideoCapture('out.mkv')
 fourcc = cv2.VideoWriter_fourcc(*'I420')
 out = cv2.VideoWriter('/x/test.avi',fourcc,T_FPS,(1920,1080))
 
 
-chess_b = np.full((16,16),-1)
-chess_w = np.full((16,16),1)
-
+chess_b = np.full((64,64),-1)
+chess_w = np.full((64,64),0)
 chess = np.concatenate(
         [np.concatenate([chess_b,chess_w] * 64),np.concatenate([chess_w,chess_b] * 64)] * 64,
         axis = 1)
@@ -49,35 +71,84 @@ chess = chess[0:CH,0:CW]
 chess_inv = chess_inv[0:CH,0:CW]
 chess_r = [chess,chess_inv]
 
-seq = ([0,1] * 30 + [-1] * 60) * 100000
-index = 0
+def gs(X,row_vecs = True,norm = True):
+    if not row_vecs:
+        X = X.T
+    Y = X[0:1,:].copy()
+    for i in range(1, X.shape[0]):
+        proj = np.diag((X[i,:].dot(Y.T)/np.linalg.norm(Y,axis=1)**2).flat).dot(Y)
+        Y = np.vstack((Y, X[i,:] - proj.sum(0)))
+    if norm:
+        Y = np.diag(1/np.linalg.norm(Y,axis=1)).dot(Y)
+    if row_vecs:
+        return Y
+    else:
+        return Y.T
+def genpat(pattern):
+    d = np.zeros((120,120),dtype = np.float)
+    index = 0
+    for y in range(120):
+        for x in range(120):
+            if (x + y) >= 0 and (x + y) < 30:
+                d[y,x] = pattern[index]
+                index += 1
+    return d
 
+np.random.seed(23)
+keys = []
+for j in range(256):
+    v = []
+    for i in range((1 + 30) * 30 // 2):
+        v.append(np.random.normal(0,10000))
+    keys.append(v)
+keys = np.array(keys)
+keys = gs(keys) * 1000
+kframes = []
+for key in keys:
+    print(np.max(key))
+    print(np.min(key))
+    d = genpat(key)
+    kframe = cv2.dct(d,cv2.DCT_INVERSE)
+    kframes.append(kframe)
+    print(np.max(kframe))
+    print(np.min(kframe))
+    #cv2.imshow('d',(kframe + 128).astype('uint8'))
+    #cv2.waitKey(0)
+
+data = [0,5,7,3] * 20
+dframe = np.zeros((CH,CW))
+index = 0
+for y in range(0,CH,120):
+    for x in range(0,CW,120):
+        dframe[y:y + 120,x:x + 120] = kframes[data[index]]
+        index += 1
+#cv2.imshow('d',(dframe + 128).astype('uint8'))
+#cv2.waitKey(0)
+
+
+seq = ([-1,1] * 30) * 100000
+index = 0
 while index < 3000:
     _,sframe = invc.read()
     if sframe is None:
         break
-    sframe = cv2.resize(sframe,(CW,CH))
+    sframe = sframe[CPAD_H:CPAD_H + CH,CPAD_W:CPAD_W + CW]
     for repeat in range(T_FPS // O_FPS):
         print(index)
         frame = cv2.cvtColor(sframe,cv2.COLOR_RGB2HSV).astype(np.int)
 
-        if seq[index] != -1:
-            ch = chess_r[seq[index]]
-            bri = frame[:,:,2]
-            sta = frame[:,:,1]
-            bri = np.clip(bri + ch * 30,0,255)
-            sta2 = np.clip(sta + bri * 0.05 * ch,0,255)
-            #sta2 = np.clip(sta + (((bri - 128) // 32) ** 5) * ch * 0.1,0,255)
-            frame[:,:,2] = bri
-            #cv2.imshow('b',cv2.cvtColor(frame.astype(np.uint8),cv2.COLOR_HSV2RGB))
-            #frame[:,:,1] = sta1
-            #cv2.imshow('x1',cv2.cvtColor(frame.astype(np.uint8),cv2.COLOR_HSV2RGB))
-            frame[:,:,1] = sta2
+        #ch = chess_r[seq[index]]
+        ch = dframe * seq[index]
+        bri = frame[:,:,2]
+        sta = frame[:,:,1]
+        bri = np.clip(bri * 0.8 + 25.6 + ch,0,255)
+        #sta2 = np.clip(sta + bri * 0.05 * ch,0,255)
+        frame[:,:,2] = bri
         frame = cv2.cvtColor(frame.astype(np.uint8),cv2.COLOR_HSV2RGB)
-        #cv2.imshow('x2',frame)
-        #cv2.waitKey(0)
 
         i_frame[CPAD_H:H - CPAD_H,CPAD_W:W - CPAD_W] = frame
+        #cv2.imshow('x2',i_frame)
+        #cv2.waitKey(0)
         out.write(i_frame)
         index += 1
 
